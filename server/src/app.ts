61f04867948d1c05a7e484798808e19c45af9b6c
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-07-13 01:27:54 -0700

    added login route and redirects to callback url

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,12 +7,39 @@ dotenv.config()
 const app: Application = express()
 const router: Router = express.Router()
 
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectUri: 'http://localhost:5000/callback',
+})
+
 router.get('/', (req: Request, res: Response) => {
   res.send('Home')
 })
 
 router.get('/login', (req, res) => {
-  res.send('Login')
+  const scopes = [
+    'user-read-private',
+    'user-read-email',
+    'playlist-modify-public',
+    'playlist-modify-private',
+  ]
+  var authorizeURL = spotifyApi.createAuthorizeURL(scopes, 'loggedin')
+  res.redirect(authorizeURL)
+
+  // res.redirect(
+  //   'https://accounts.spotify.com/authorize' +
+  //     '?response_type=code' +
+  //     '&client_id=' +
+  //     process.env.CLIENT_ID +
+  //     (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+  //     '&redirect_uri=' +
+  //     encodeURIComponent('http://localhost:5000/')
+  // )
+})
+
+router.get('/callback', (req: Request, res: Response) => {
+  res.send('Callback')
 })
 
 app.use('/', router)
