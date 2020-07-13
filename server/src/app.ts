198a31722147cb5e249a259e2ba27043b5faf0a7
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-07-13 00:37:45 -0700

    installed spotify web api node

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,13 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application, Request, Response, Router } from 'express'
+import SpotifyWebApi from 'spotify-web-api-node'
 
 const app: Application = express()
+const router: Router = express.Router()
 
-app.get('/', (req: Request, res: Response) => {
+router.get('/hello', (req: Request, res: Response) => {
   res.send('Hello World')
 })
+
+app.use('/api', router)
 
 app.listen(5000, () => console.log('Server running port 5000!'))
