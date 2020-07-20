5cb3de0cf70691e672c8eab15d3ece03f982bc37
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-07-19 22:06:34 -0700

    added api function to fetch playlists

--- client/src/api/api.tsx
+++ client/src/api/api.tsx
@@ -2,18 +2,19 @@ import { useState } from 'react'
 import axios, { AxiosResponse } from 'axios'
 import useAsyncEffect from 'use-async-effect'
 
-const getPlaylists = () => {
-  const [data, setData] = useState({})
+const GetPlaylists = () => {
+  const [tracksData, setTracksData] = useState({ items: { tracks: {} } })
+  const [imageData, setImageData] = useState({ items: { images: {} } })
 
   useAsyncEffect(async () => {
     const result: AxiosResponse<any> = await axios(
       'http://localhost:5000/playlists'
     )
-
-    setData(result.data)
+    setTracksData(result.data.items[0].tracks.href)
+    setImageData(result.data.items[0].images[0].url)
   }, [])
-
-  return data
+  console.log(tracksData, imageData)
+  return { tracksData, imageData }
 }
 
-export default getPlaylists
+export default GetPlaylists
